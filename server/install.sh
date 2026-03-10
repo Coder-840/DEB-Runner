9f3f73dc87b8cb4643e8065314ea9ce47e634ca0
Coder-840 <rhcoding22@gmail.com>
2026-03-09 23:28:41 -0400

    Update install.sh

--- server/install.sh
+++ server/install.sh
@@ -5,11 +5,12 @@ export DISPLAY=:1
 mkdir -p /tmp/.X11-unix
 
 Xvfb :1 -screen 0 1280x720x24 &
-
 sleep 2
 
 fluxbox &
+sleep 1
 
-x11vnc -display :1 -nopw -forever -shared &
+x11vnc -create -forever -shared -nopw -display :1 &
+sleep 1
 
 websockify --web /usr/share/novnc/ 6080 localhost:5900
